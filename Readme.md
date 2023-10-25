ddfdb08e08515a84cc1795b38c1fe1c5849a3efa
Nitin Chaurasia <gs.nitin@gmail.com>
2023-10-25 08:53:28 -0600

    producer

---
+++ Readme.md
@@ -0,0 +1,16 @@
+
+```shell
+cd /Applications/Development/kafka_2.13-3.6.0
+bin/zookeeper-server-start.sh config/zookeeper.properties
+```
+
+In another tab
+
+```shell
+cd /Applications/Development/kafka_2.13-3.6.0
+bin/kafka-server-start.sh config/server.properties
+```
+
+## Conduktor
+
+Download and install and run once the kafka server is running on local and 
---
+++ src/main/java/com/learn/kafka/controller/KafkaController.java
@@ -0,0 +1,20 @@
+package com.learn.kafka.controller;
+
+import com.learn.kafka.service.KafkaProducerService;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class KafkaController {
+    private final KafkaProducerService producerService;
+
+    public KafkaController(KafkaProducerService producerService) {
+        this.producerService = producerService;
+    }
+
+    @PostMapping("/messages")
+    public void sendMessageToKafka(@RequestBody String message) {
+        producerService.sendMessage(message);
+    }
+}
