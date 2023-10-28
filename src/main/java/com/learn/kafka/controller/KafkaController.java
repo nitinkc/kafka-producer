19406b98b3220355cbda1b9bcdf9ef255afb67ce
Nitin Chaurasia <gs.nitin@gmail.com>
2023-10-27 22:42:34 -0600

    Completable future added

--- src/main/java/com/learn/kafka/controller/KafkaController.java
+++ src/main/java/com/learn/kafka/controller/KafkaController.java
@@ -2,7 +2,7 @@ package com.learn.kafka.controller;
 
 import com.learn.kafka.model.StudentDto;
 import com.learn.kafka.service.KafkaSimpleProducerService;
-import com.learn.kafka.service.KafkaStudentProducerService;
+import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RestController;
@@ -10,20 +10,20 @@ import org.springframework.web.bind.annotation.RestController;
 @RestController
 public class KafkaController {
     private final KafkaSimpleProducerService producerService;
-    private final KafkaStudentProducerService studentProducerService;
 
-    public KafkaController(KafkaSimpleProducerService producerService, KafkaStudentProducerService studentProducerService) {
+    public KafkaController(KafkaSimpleProducerService producerService) {
         this.producerService = producerService;
-        this.studentProducerService = studentProducerService;
     }
 
     @PostMapping("/messages.simple.topic")
-    public void sendMessageToKafka(@RequestBody String message) {
-        producerService.sendMessage(message);
+    public ResponseEntity<String> sendMessageToKafka(@RequestBody String message) {
+
+        return ResponseEntity.ok(producerService.sendSimpleTextMessage(message));
+
     }
 
     @PostMapping("/student.topic")
-    public String sendMessageToKafka(@RequestBody StudentDto message) {
-        return producerService.sendStudentMessage(message);
+    public ResponseEntity<String> sendMessageToKafka(@RequestBody StudentDto message) {
+        return ResponseEntity.ok(producerService.sendStudentMessage(message));
     }
 }
--- src/main/java/com/learn/kafka/service/KafkaSimpleProducerService.java
+++ src/main/java/com/learn/kafka/service/KafkaSimpleProducerService.java
@@ -10,16 +10,15 @@ import org.springframework.kafka.support.SendResult;
 import org.springframework.stereotype.Service;
 
 import java.util.concurrent.CompletableFuture;
+import java.util.concurrent.ExecutionException;
 
 @Service
 public class KafkaSimpleProducerService {
     private final KafkaTemplate<String, String> kafkaTemplate;
     private final ObjectMapper objectMapper;
-    @Value("${com.topic.simple}")
-    private String simpleTopic;
 
-    @Value("${com.topic.student}")
-    private String studentTopic;
+    @Value("${com.topic.simple}") private String simpleTopic;
+    @Value("${com.topic.student}") private String studentTopic;
 
     @Autowired
     public KafkaSimpleProducerService(KafkaTemplate<String, String> kafkaTemplate, ObjectMapper objectMapper) {
@@ -27,30 +26,43 @@ public class KafkaSimpleProducerService {
         this.objectMapper = objectMapper;
     }
 
-    public void sendMessage(String message) {
+    public String sendSimpleTextMessage(String message) {
         CompletableFuture<SendResult<String, String>> future = kafkaTemplate.send(simpleTopic, message);
-        handleSuccessOrException(message, future);
+        CompletableFuture<Void> voidCompletableFuture = handleSuccessOrException(message, future)
+                .thenAccept(result -> {
+                    System.out.println("Received final result: " + result);
+        });
+
+        String str = "Simple Message Sent to Topic :: " + simpleTopic;
+
+        voidCompletableFuture.thenRun(() -> System.out.println(str));
+
+        return str;
     }
 
     public String sendStudentMessage(StudentDto message) {
-        String ret;
+        String ret = "";
         try {
             String messageJson = objectMapper.writeValueAsString(message); // Convert StudentDto to JSON
             CompletableFuture<SendResult<String, String>> future = kafkaTemplate.send(studentTopic, messageJson);
-            handleSuccessOrException(messageJson, future);
-        } catch (JsonProcessingException e) {
+            CompletableFuture<String> stringCompletableFuture = handleSuccessOrException(messageJson, future);
+
+            ret =  stringCompletableFuture.get();
+        } catch (JsonProcessingException | InterruptedException | ExecutionException e) {
             System.out.println("Error serializing StudentDto to JSON: " + e.getMessage());
         }
-        return message.getFullName();
+        return ret;
     }
 
-    private static void handleSuccessOrException(String message, CompletableFuture<SendResult<String, String>> future) {
-        future.whenComplete((result, ex) -> {
-            if (ex == null) {
-                System.out.println("Sent message=[" + message + "] with offset=[" + result.getRecordMetadata().offset() + "]");
-            } else {
-                System.out.println("Unable to send message=[" + message + "] due to : " + ex.getMessage());
-            }
-        });
+    private static CompletableFuture<String> handleSuccessOrException(String message, CompletableFuture<SendResult<String, String>> future) {
+        return future
+                .thenApply(result -> "Sent message=[" + message + "] with offset=[" + result.getRecordMetadata().offset() + "]" + result.getProducerRecord().headers())
+                .exceptionally(ex -> "Unable to send message=[" + message + "] due to: " + ex.getMessage())
+                .thenApplyAsync(resultMessage -> {
+                    // Perform additional asynchronous processing
+                    String processedResult = resultMessage + " - Additional Processing";
+                    System.out.println(processedResult);
+                    return processedResult;
+                });
     }
 }
